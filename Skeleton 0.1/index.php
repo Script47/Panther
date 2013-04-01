20f1ad2ee32a3247b1889a655ed2cb17c5058723
Harry Denley <h.denley@yahoo.co.uk>
2013-04-01 19:43:17 +0100

    'optimized' index file
    (Thanks k1ngscorp1o for the tip)

--- Skeleton 0.1/index.php
+++ Skeleton 0.1/index.php
@@ -16,20 +16,15 @@ $template->set('title', $config['site_title']);
 $template->set('footer', $config['footer']);
 
 if (array_key_exists('page', $_GET)) {
-    $_GET['page'] = filter_var($_GET['page'], FILTER_SANITIZE_STRING) ? htmlspecialchars($_GET['page']) : '';
-    if (in_array($_GET['page'], scandir('mods', 1))) {
-        ob_start();
-        include_once('mods/' . $_GET['page'] . '/main.php');
-        $file = ob_get_contents();
-        ob_end_clean();
-        $temp['content'] = $file;
-    } else {
-        ob_start();
-        include_once('mods/index/main.php');
-        $file = ob_get_contents();
-        ob_end_clean();
-        $temp['content'] = $file;
-    }
+
+    $page = filter_var($_GET['page'], FILTER_SANITIZE_STRING) ? htmlspecialchars($_GET['page']) : '';
+    $page = in_array($page, scandir('mods', 1)) ? $page : 'index';
+
+    ob_start();
+    include_once('mods/' . $page . '/main.php');
+    $file = ob_get_contents();
+    ob_end_clean();
+    $temp['content'] = $file;
 } else {
     ob_start();
     include_once('mods/index/main.php');
@@ -39,20 +34,13 @@ if (array_key_exists('page', $_GET)) {
 }
 
 
+$menu = array_key_exists('loggedin', $_SESSION) ? 'home' : 'index';
 //Fetch the menu data :)
-if ( array_key_exists('loggedin', $_SESSION) ) {
-    ob_start();
-    include_once('mods/home/menu.php');
-    $file = ob_get_contents();
-    ob_end_clean();
-    $temp['menu'] = $file;
-} else {
-    ob_start();
-    include_once('mods/index/menu.php');
-    $file = ob_get_contents();
-    ob_end_clean();
-    $temp['menu'] = $file;  
-}
+ob_start();
+include_once('mods/' . $menu . '/menu.php');
+$file = ob_get_contents();
+ob_end_clean();
+$temp['menu'] = $file;
 
 $template->set('menu', $temp['menu']);
 $template->set('content', $temp['content']);
